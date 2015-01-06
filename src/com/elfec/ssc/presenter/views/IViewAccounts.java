a94284f4a3c276e25ea4c1618dc32884c3414c23
zuki26 <nelson-araoz@hotmail.com>
2015-01-06 17:32:25 -0400

    view accounts

--- src/com/elfec/ssc/presenter/views/IViewAccounts.java
+++ src/com/elfec/ssc/presenter/views/IViewAccounts.java
@@ -1,6 +1,10 @@
 package com.elfec.ssc.presenter.views;
 
+import java.util.List;
+
+import com.elfec.ssc.model.Account;
+
 public interface IViewAccounts {
 	//temporal
-	public void show(String message);
+	public void show(List<Account> result);
 }
