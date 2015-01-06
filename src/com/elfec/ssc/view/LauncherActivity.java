52dc68c999aca929c6c886220f3a6dc2be2a2326
diegoRodriguezAguila <diroag@gmail.com>
2015-01-06 16:34:18 -0400

    MainMenu

---
+++ src/com/elfec/ssc/view/LauncherActivity.java
@@ -0,0 +1,26 @@
+package com.elfec.ssc.view;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class LauncherActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Intent i;
+		if(true)
+		{
+			i = new Intent(LauncherActivity.this, Welcome.class);
+			
+		}
+		else
+		{
+			i = new Intent(LauncherActivity.this, MainMenu.class);
+		}
+		i.addFlags( Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+		startActivity(i);
+		LauncherActivity.this.finish();
+	}
+}
