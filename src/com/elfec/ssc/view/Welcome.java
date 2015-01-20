965da1e79dcb3b143d76c202aa6f46ee41a11358
diegoRodriguezAguila <diroag@gmail.com>
2015-01-20 14:50:05 -0400

    iconito para configurar nivel de cercanía para puntos cercanos

--- src/com/elfec/ssc/view/Welcome.java
+++ src/com/elfec/ssc/view/Welcome.java
@@ -65,10 +65,6 @@ public class Welcome extends ActionBarActivity implements IWelcome {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
 		return super.onOptionsItemSelected(item);
 	}
 	
