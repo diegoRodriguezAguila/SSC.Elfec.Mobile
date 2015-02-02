db5f6ef57a92c04224f9716a244345abbe14c77f
zuki26 <nelson-araoz@hotmail.com>
2015-02-02 17:15:01 -0400

    eliminar notificaciones

--- src/com/elfec/ssc/businesslogic/ElfecNotificationManager.java
+++ src/com/elfec/ssc/businesslogic/ElfecNotificationManager.java
@@ -1,8 +1,15 @@
 package com.elfec.ssc.businesslogic;
 
+import com.activeandroid.query.Delete;
+import com.elfec.ssc.model.Notification;
+
 
 
 public class ElfecNotificationManager {
 
+	public static void removeAllNotifications(short type)
+	{
+		new Delete().from(Notification.class).where("Type=?",type).execute();
+	}
 
 }
