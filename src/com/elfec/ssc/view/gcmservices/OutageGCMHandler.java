314424d052b862f049e69b6f052dd80dc5cc56ee
diegoRodriguezAguila <diroag@gmail.com>
2015-02-19 10:33:34 -0400

    Se puso condición para que las notificaciones de cortes no lleguen si es que esta con otro gmail

--- src/com/elfec/ssc/view/gcmservices/OutageGCMHandler.java
+++ src/com/elfec/ssc/view/gcmservices/OutageGCMHandler.java
@@ -2,7 +2,9 @@ package com.elfec.ssc.view.gcmservices;
 
 import com.elfec.ssc.businesslogic.ElfecNotificationManager;
 import com.elfec.ssc.helpers.ViewPresenterManager;
+import com.elfec.ssc.model.Client;
 import com.elfec.ssc.model.Notification;
+import com.elfec.ssc.model.enums.ClientStatus;
 import com.elfec.ssc.model.enums.NotificationKey;
 import com.elfec.ssc.model.enums.NotificationType;
 import com.elfec.ssc.presenter.ViewNotificationsPresenter;
@@ -24,14 +26,18 @@ public class OutageGCMHandler implements IGCMHandler {
 	@Override
 	public void handleGCMessage(Bundle messageInfo,
 			NotificationManager notifManager, Builder builder) {
-		Notification notif = ElfecNotificationManager.SaveNotification(messageInfo.getString("title"), messageInfo.getString("message"),
-				NotificationType.get(Short.parseShort(messageInfo.getString("type"))), NotificationKey.get(messageInfo.getString("key")));
-		//Si la vista de ver notificaciones está activa
-		ViewNotificationsPresenter notifPresenter = ViewPresenterManager
-				.getPresenter(ViewNotificationsPresenter.class);
-		if (notifPresenter != null)
-			notifPresenter.addNewOutageNotificationUpdate(notif);
-		notifManager.notify(NOTIF_ID, builder.build());
+		Client ownerClient = Client.getClientByGmail(messageInfo.getString("gmail"));
+		if(ownerClient != null && ownerClient.getStatus()==ClientStatus.ACTIVE)
+		{
+			Notification notif = ElfecNotificationManager.SaveNotification(messageInfo.getString("title"), messageInfo.getString("message"),
+					NotificationType.get(Short.parseShort(messageInfo.getString("type"))), NotificationKey.get(messageInfo.getString("key")));
+			//Si la vista de ver notificaciones está activa
+			ViewNotificationsPresenter notifPresenter = ViewPresenterManager
+					.getPresenter(ViewNotificationsPresenter.class);
+			if (notifPresenter != null)
+				notifPresenter.addNewOutageNotificationUpdate(notif);
+			notifManager.notify(NOTIF_ID, builder.build());
+		}
 	}
 
 	@Override
