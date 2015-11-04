e4e43f8ba1cab2aebe95a01f6e7ba624987c47a0
diegoRodriguezAguila <diroag@gmail.com>
2015-11-04 16:23:00 -0400

    Cambiados selectores de texto para que sean material. Modificada la forma de renderizado de notificaciones para seguir el patron viewholder. Agregado tipo de notificación de facturas vencidas

---
+++ src/com/elfec/ssc/view/adapters/viewholders/NotificationViewHolder.java
@@ -0,0 +1,58 @@
+package com.elfec.ssc.view.adapters.viewholders;
+
+import android.text.Html;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.elfec.ssc.R;
+import com.elfec.ssc.helpers.utils.NotificationDrawablePicker;
+import com.elfec.ssc.model.Notification;
+import com.elfec.ssc.view.adapters.ExpandableItem;
+
+/**
+ * ViewHolder para notificaciones
+ * 
+ * @author drodriguez
+ *
+ */
+public class NotificationViewHolder {
+	public TextView lblMessage;
+	public ImageView imgNotification;
+	private TextView lblTitle;
+	private TextView lblDate;
+	private TextView lblHour;
+
+	public NotificationViewHolder(View convertView) {
+		lblMessage = (TextView) convertView
+				.findViewById(R.id.list_item_notification_message);
+		imgNotification = (ImageView) convertView
+				.findViewById(R.id.notification_image);
+		lblTitle = (TextView) convertView
+				.findViewById(R.id.list_item_notification_title);
+		lblDate = (TextView) convertView
+				.findViewById(R.id.list_item_notification_date);
+		lblHour = (TextView) convertView
+				.findViewById(R.id.list_item_notification_time);
+	}
+
+	/**
+	 * Asigna la información a los campos de la vista
+	 * 
+	 * @param notification
+	 */
+	public void bindNotification(Notification notification,
+			ExpandableItem expandedStatus) {
+		int endSize = 0;
+		if (expandedStatus.isExpanded()) {
+			endSize = expandedStatus.getExpandedSize();
+		}
+		lblMessage.getLayoutParams().height = endSize;
+		lblMessage.setText(Html.fromHtml(notification.getContent()));
+		imgNotification.setImageResource(NotificationDrawablePicker
+				.getDrawable(notification.getKey()));
+		lblTitle.setText(Html.fromHtml(notification.getTitle()));
+		lblDate.setText(notification.getInsertDate().toString("dd MMM yyyy"));
+		lblHour.setText(notification.getInsertDate().toString("HH:mm"));
+	}
+}
